7762c5bc993cf612983e60437a7551bf77a0ad0b
malikzzz <mbabazov@list.ru>
2025-02-10 18:52:56 +0600

    create seo-meta

--- pages/contacts/index.vue
+++ pages/contacts/index.vue
@@ -1,4 +1,13 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  useSeoMeta({
+    title: 'Наши контакты',
+    ogTitle: 'Наши контакты',
+    description: 'Это мой невероятный сайт, позвольте мне рассказать вам о нем.',
+    ogDescription: 'Это мой невероятный сайт, позвольте мне рассказать вам о нем.',
+    ogImage: 'https://example.com/image.png',
+    twitterCard: 'summary_large_image',
+  })
+</script>
 
 <template>
   <div>Contacts</div>
