bdfe6414583f793095063b30f3d88b3f13e267f7
dreamprogrammin <mbabazov@list.ru>
2025-02-19 07:58:01 +0500

    create auth

--- plugins/supabase.client.ts
+++ plugins/supabase.client.ts
@@ -1,10 +1,16 @@
-import { createClient } from "@supabase/supabase-js"
+import { createClient } from "@supabase/supabase-js";
+import { useAuthStore } from "~/stores/auth";
 
-const supabaseUrl = process.env.NUXT_PUBLIC_SUPABASE_URL! 
-const supabaseKey = process.env.NUXT_PUBLIC_SUPABASE_KEY!
+const supabaseUrl = process.env.NUXT_PUBLIC_SUPABASE_URL!;
+const supabaseKey = process.env.NUXT_PUBLIC_SUPABASE_KEY!;
 
+const authStore = useAuthStore();
 
 export default defineNuxtPlugin((nuxtApp) => {
-    const supabase = createClient(supabaseUrl, supabaseKey)
-    nuxtApp.provide('supabase', supabase)
-})
+  const supabase = createClient(supabaseUrl, supabaseKey);
+  supabase.auth.onAuthStateChange((event, session) => {
+    if (event === "SIGNED_IN") {
+    }
+  });
+  nuxtApp.provide("supabase", supabase);
+});
