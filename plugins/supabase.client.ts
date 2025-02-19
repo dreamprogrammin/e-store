8f7db1759c2784785c510ec9427e32ca8a221e9c
dreamprogrammin <mbabazov@list.ru>
2025-02-19 08:00:19 +0500

    create auth part 2

--- plugins/supabase.client.ts
+++ plugins/supabase.client.ts
@@ -10,6 +10,10 @@ export default defineNuxtPlugin((nuxtApp) => {
   const supabase = createClient(supabaseUrl, supabaseKey);
   supabase.auth.onAuthStateChange((event, session) => {
     if (event === "SIGNED_IN") {
+      authStore.setUser(session?.user || null);
+    } else if (event === "SIGNED_OUT") {
+      authStore.clearUser();
+      console.log("hi");
     }
   });
   nuxtApp.provide("supabase", supabase);
